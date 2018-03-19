e255afbff65789323bd61b6600c8cb1541fc4b91
Ania <ania.makes@gmail.com>
2018-03-19 11:38:22 +0000

    added Transaction component. Transaction history is now displayed

--- src/App.js
+++ src/App.js
@@ -1,20 +1,56 @@
 import React from 'react';
 import Input from "./Input";
+import Transaction from "./Transaction";
 
 class App extends React.Component {
   constructor(){
     super();
+
+    this.state = {
+      transactionHistory: [],
+      id : 0
+    };
+
+    this.bookTransaction = this.bookTransaction.bind(this);
   }
 
   bookTransaction(incomingTransaction){
     console.log("I've made it to bookTransaction");
     console.log(incomingTransaction);
+
+    incomingTransaction.key = this.state.id;
+
+    this.state.transactionHistory.push(incomingTransaction);
+
+    this.setState({
+      id: this.state.id + 1
+    });
   }
 
   render(){
+
+    console.log(this.state.transactionHistory);
+
+    let workingTransactionHistory = [].concat(this.state.transactionHistory);
+    workingTransactionHistory.sort(function(a,b){
+      const dateA = new Date(a.date);
+      const dateB = new Date(b.date);
+      return dateB - dateA;
+    });
+
+    console.log(this.state.workingTransactionHistory);
+
+    let outputTransactionHistory = workingTransactionHistory.map(function(item){
+      return <Transaction description={item.description} date={item.date} amount={item.amount} type={item.type} key={item.key}/>;
+
+    });
+
     return (
       <div>
         <Input transactionReceiver={this.bookTransaction}/>
+        <div>
+        {outputTransactionHistory}
+        </div>
       </div>
     );
   }
---
+++ src/Transaction.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+class Transaction extends React.Component{
+  constructor(){
+    super();
+
+
+}
+
+  render(){
+
+    const {description, date, amount, type} = this.props;
+
+    return(
+      <div>
+        <p>{description}</p>
+        <p>{date}</p>
+        <p>{amount}</p>
+        <p>{type}</p>
+      </div>
+    );
+
+  }
+}
+
+export default Transaction;
