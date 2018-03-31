79b60174cfd6fabee2d4af9a43121dbcee1da12f
Ania <ania.makes@gmail.com>
2018-03-31 22:11:02 +0100

    transactions now saved to localstorage and assign key correctly

--- src/App.js
+++ src/App.js
@@ -12,43 +12,120 @@ class App extends React.Component {
     };
 
     this.bookTransaction = this.bookTransaction.bind(this);
+	this.fetchTransactionsFromMemory = this.fetchTransactionsFromMemory.bind(this);
+    this.updateHistory = this.updateHistory.bind(this);
+    this.obtainIdToUseForIncomingTransaction = this.obtainIdToUseForIncomingTransaction.bind(this);
   }
+
+  obtainIdToUseForIncomingTransaction(){
+      let objectTransactionsFromMemory = this.fetchTransactionsFromMemory();
+
+      let last = 0;
+      if(objectTransactionsFromMemory){
+          if(objectTransactionsFromMemory.history.length > 1){
+              objectTransactionsFromMemory.history.sort(function(a,b){
+                  const alpha = a.key;
+                  const bravo = b.key;
+                  return alpha - bravo;
+              });
+
+              last = objectTransactionsFromMemory.history[objectTransactionsFromMemory.history.length-1].key + 1;
+          } else {
+             last = 1;
+          }
+      }
+      return last;
+}
+
+  fetchTransactionsFromMemory(){
+	  let transactionsFromMemory = localStorage.getItem("memory");
+      console.log(transactionsFromMemory);
+      return JSON.parse(transactionsFromMemory);
+  };
 
   bookTransaction(incomingTransaction){
     console.log("I've made it to bookTransaction");
     console.log(incomingTransaction);
 
-    incomingTransaction.key = this.state.id;
-
-    this.state.transactionHistory.push(incomingTransaction);
-
-    this.setState({
-      id: this.state.id + 1
-    });
-
-    let toHistory = JSON.stringify({"history" : this.state.transactionHistory});
-
-    localStorage.setItem("memory", toHistory);
-
+    this.updateHistory(this.obtainIdToUseForIncomingTransaction(), incomingTransaction);
+    // if(objectTransactionsFromMemory){
+    //     let last = 0;
+    //     if(objectTransactionsFromMemory.history.length > 1){
+    //         objectTransactionsFromMemory.history.sort(function(a,b){
+    //             const alpha = a.key;
+    //             const bravo = b.key;
+    //             return alpha - bravo;
+    //         });
+    //
+    //         last = objectTransactionsFromMemory.history[objectTransactionsFromMemory.history.length-1].key;
+    //     }
+    //     console.log('1 ', this.state);
+    //     console.log(last + 1);
+    //     const newId = last + 1;
+    //     this.setState({
+    //         id: newId
+    //     });
+    //
+    //     console.log('2 ', this.state);
+    // } else {
+    // let objectTransactionsFromMemory = this.fetchTransactionsFromMemory();
+    //
+    // if (!objectTransactionsFromMemory) {
+    //     objectTransactionsFromMemory = {history : []};
+    // }
+    // // }
+    // console.log(objectTransactionsFromMemory);
+    // // console.log('3 ', this.state);
+    // incomingTransaction.key = this.obtainIdToUseForIncomingTransaction();
+    // objectTransactionsFromMemory.history.push(incomingTransaction);
+    //
+    //
+    // // this.setState({
+    // //   id: this.state.id + 1
+    // // });
+    //
+    // let toHistory = JSON.stringify({"history" : objectTransactionsFromMemory.history});
+    // localStorage.setItem("memory", toHistory);
   }
+
+  updateHistory(lastUsedId, incomingTransaction){
+      let objectTransactionsFromMemory = this.fetchTransactionsFromMemory();
+
+      if (!objectTransactionsFromMemory) {
+          objectTransactionsFromMemory = {history : []};
+      }
+      console.log(objectTransactionsFromMemory);
+      incomingTransaction.key = this.obtainIdToUseForIncomingTransaction();
+      objectTransactionsFromMemory.history.push(incomingTransaction);
+      let toHistory = JSON.stringify({"history" : objectTransactionsFromMemory.history});
+      localStorage.setItem("memory", toHistory);
+  }
+
 
   render(){
 
-    console.log(this.state.transactionHistory);
+    console.log('state ', this.state.transactionHistory);
+    console.log('localStorage', this.fetchTransactionsFromMemory());
 
-    let workingTransactionHistory = [].concat(this.state.transactionHistory);
-    workingTransactionHistory.sort(function(a,b){
-      const dateA = new Date(a.date);
-      const dateB = new Date(b.date);
-      return dateB - dateA;
-    });
+    const transactionsFromMemory = this.fetchTransactionsFromMemory();
 
-    console.log(this.state.workingTransactionHistory);
+    let workingTransactionHistory;
+    let outputTransactionHistory;
 
-    let outputTransactionHistory = workingTransactionHistory.map(function(item){
-      return <Transaction description={item.description} date={item.date} amount={item.amount} type={item.type} key={item.key}/>;
+    if(transactionsFromMemory){
+        workingTransactionHistory = [].concat(transactionsFromMemory.history);
+        workingTransactionHistory.sort(function(a,b){
+            const dateA = new Date(a.date);
+            const dateB = new Date(b.date);
+            return dateB - dateA;
+        });
 
-    });
+        console.log(workingTransactionHistory);
+
+        outputTransactionHistory = workingTransactionHistory.map(function(item){
+            return <Transaction description={item.description} date={item.date} amount={item.amount} type={item.type} key={item.key}/>;
+        });
+};
 
     return (
       <div>
